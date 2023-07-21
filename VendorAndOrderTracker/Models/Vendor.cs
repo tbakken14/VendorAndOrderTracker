bb0b6254e659f055580db3ffccb6db612118a72e
Thomas Bakken <tbakken14@gmail.com>
2023-07-21 13:16:43 -0700

    Add vendor model functionality

--- VendorAndOrderTracker/Models/Vendor.cs
+++ VendorAndOrderTracker/Models/Vendor.cs
@@ -1,5 +1,18 @@
-namespace VendorAndOrderTracker.Models {
-    public class Vendor {
-        
+using System.Collections.Generic;
+
+namespace VendorAndOrderTracker.Models
+{
+    public class Vendor
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public List<Order> Orders { get; set; }
+
+        public Vendor(string name, string description)
+        {
+            Name = name;
+            Description = description;
+            Orders = new List<Order>();
+        }
     }
 }
