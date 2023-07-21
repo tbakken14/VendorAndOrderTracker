1372baad2ae02274924f3dd7dd74781afaff0aaf
Thomas Bakken <tbakken14@gmail.com>
2023-07-21 15:21:19 -0700

    Add home controller index route

--- VendorAndOrderTracker/Controllers/HomeController.cs
+++ VendorAndOrderTracker/Controllers/HomeController.cs
@@ -1,8 +1,14 @@
 using Microsoft.AspNetCore.Mvc;
 using VendorAndOrderTracker.Models;
 
-namespace VendorAndOrderTracker.Controllers {
-    public class HomeController : Controller {
-
+namespace VendorAndOrderTracker.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet("/")]
+        public ActionResult Index()
+        {
+            return View();
+        }
     }
 }
